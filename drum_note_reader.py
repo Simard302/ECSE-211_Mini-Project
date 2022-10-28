9119f87919d019e59530b9d4f2c3bb09e3abb3d0
Adam Simard <asimard@congruencetx.com>
2022-10-28 12:58:29 -0400

    added comments, changed sound module

--- drum_note_reader.py
+++ drum_note_reader.py
@@ -1,10 +1,10 @@
 import code
 from utils.brick import Motor, TouchSensor, TouchSensor
-from utils import sound
+from utils.sound import NOTES, gen_wave
 import time
 from multiprocessing import Process
 from threading import Thread
-import os
+import simpleaudio as sa
 
 touch1 = TouchSensor(1)
 touch2 = TouchSensor(2)
@@ -24,8 +24,6 @@ note_duration = 0.1
 note_hold_extra_time = 3
 
 drum_pattern = "kkc "
-flute_pattern_time = [1, 1, 1, 1, 1/2, 3.5]
-flute_pattern = ["G5", "Gb5", "E5", "D5", "E5", "E5"]
 
 KICK_POWER = 100 #full power kick motor
 CLAP_POWER = 100 #full power clap motor
@@ -34,13 +32,15 @@ BPM = bpm_setter.get_position() #default value of bpm radial setter (lowest BPM)
 SPEED = BPM*6 # Conversion of BPM to Deg --> SPEED = BPM * 360degrees / 60seconds
         
 def play_drum(drum):
-    #same bit of code as the other function, the idea is to allow BPM changes will the kicks and claps are playing
+    # Get BPM from motor position
     BPM = bpm_setter.get_position()
+    # Clamp BPM to avoid
     if(BPM<=60):
         BPM=60
 
     if(BPM>=200):
         BPM=200
+    print(BPM)
         
     SPEED = BPM*6*hit_factor
     
@@ -63,9 +63,24 @@ def play_drum(drum):
 def play_drums():
     #string of notes representing kicks and claps
     for character in drum_pattern:
+        # Play individual drum sound
         play_drum(character)
-        #play_note(flute_note)
+    # Re-loop drumming (recursive)
     play_drums()
+
+def init_waves():
+    # Pre-generate all waves for existing notes
+    # Makes the sound playing much faster
+    note_waves = {}
+    for note in value_map.values():
+        note_waves[note] = gen_wave(
+            note_hold_extra_time*note_duration, 
+            flute_vol, 
+            NOTES[note], 
+            0,0, 0, 0, 1, 0.01, 8000
+        )
+    return note_waves
+
 
 
 flutes = [
@@ -95,6 +110,7 @@ def readButton(is_pressed, index):
 
 def readFlute():
     # Setting up lists for threads and their return values
+    # Pre-allocating space for Thread to reference
     is_pressed = [None] * len(flutes)
     threads = [None] * len(flutes)
 
@@ -106,36 +122,42 @@ def readFlute():
     # is_pressed is now updated
     for i in range(0, len(flutes)):
         threads[i].join()
+    # Format the True/False list into 0 and 1's string
     return "".join(list(map(str, list(map(int, is_pressed)))))
     
 if __name__ == "__main__":
+    waves = init_waves()
     drumProc = Process(target = play_drums)
+    player = None
     try:
         while True:
+            # Read all flute inputs (threaded)
             code = readFlute()
 
             if code == "1111":
                 if drumProc.is_alive():
                     # kill drum processes, break
                     drumProc.kill()
+                    print("Stopping all processes")
                     break
                 else:
                     # Start drum process
                     drumProc.start()
+                    print("Starting drum")
                     time.sleep(HOLD_TIME)
             elif code == "0000":
+                # No buttons pressed, add a time.sleep to slow the while loop
                 time.sleep(REFRESH_RATE_FREE)
             else:
                 # play flute sounds
                 print(code)
-                SOUND = sound.Sound(
-                    duration=note_hold_extra_time*note_duration, 
-                    pitch=value_map[code], 
-                    volume=flute_vol
-                )
-                SOUND.play()
-                #print(max((60/BPM)/2, note_duration))
-                time.sleep(max((60/BPM)/2, note_duration))
+                # If sound is already playing, stop it
+                if player is not None and player.is_playing():
+                    player.stop()
+                # Play new sound with pre-generated wave (faster)
+                player = sa.play_buffer(waves[value_map[code]], 1, 2, 8000)
+                # Sleep for duration of sound
+                time.sleep(note_hold_extra_time*note_duration)
     except BaseException as e:
         print(str(e))
         if drumProc.is_alive(): drumProc.kill()
